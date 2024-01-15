18025956e368994cfbc6a9e7231f5e59e5130724
Vedant Singh <vedantc252@gmail.com>
2024-01-16 00:34:36 +0530

    Finalised Categories Home Page

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick Your Category"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
+      ),
+    );
+  }
+}
